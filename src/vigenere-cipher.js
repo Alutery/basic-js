d310964e2e28a51b7206254e9e9fec92c196c2cb
Aliya Safina <ktotamy@gmail.com>
2020-02-22 23:39:01 +0300

    feat: add VigenereCipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,75 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isDirect = true) {
+        this.isDirect = isDirect;
+        this.n = 26;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if(message === undefined || key === undefined) throw new Error();
+        
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        let res = [];
+        let index = 0;
+
+        message = message.split('')
+        for(let i = 0; i < message.length; i++) {
+            let char = message[i];
+
+            if(!char.match(/[A-Z]/)) {
+                res.push(char);
+                continue;
+            }
+
+            let charCode = char.charCodeAt();
+            let keyCode = key[index].charCodeAt();
+
+            index = (index + 1) % key.length;
+
+            let charEncrypted = String.fromCharCode((charCode + keyCode) % this.n + 65);
+            res.push(charEncrypted);
+        }
+
+        if(!this.isDirect) {
+            res.reverse();
+        }
+
+        return res.join('');
+    }
+
+    decrypt(encryptedMessage, key) {
+        if(encryptedMessage === undefined || key === undefined) throw new Error();
+        
+        encryptedMessage = encryptedMessage.toUpperCase();
+        key = key.toUpperCase();
+
+        let res = [];
+        let index = 0;
+
+        encryptedMessage = encryptedMessage.split('')
+        for(let i = 0; i < encryptedMessage.length; i++) {
+            let char = encryptedMessage[i];
+
+            if(!char.match(/[A-Z]/)) {
+                res.push(char);
+                continue;
+            }
+
+            let charCode = char.charCodeAt();
+            let keyCode = key[index].charCodeAt();
+
+            index = (index + 1) % key.length;
+
+            let charEncrypted = String.fromCharCode((charCode + this.n - keyCode) % this.n + 65);
+            res.push(charEncrypted);
+        }
+
+        if(!this.isDirect) {
+            res.reverse();
+        }
+
+        return res.join('');
     }
 }
 
